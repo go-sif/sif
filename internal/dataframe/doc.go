e88723eeb624f41bc6f934833e5520b6625eaa0c
Sean McIntyre <s.mcintyre@xverba.ca>
2020-02-17 16:47:22 -0500

    Additional doc files

---
+++ internal/dataframe/doc.go
@@ -0,0 +1,2 @@
+// Package dataframe contains code related to the definition and execution of DataFrames
+package dataframe
---
+++ internal/partition/doc.go
@@ -0,0 +1,2 @@
+// Package partition comprises code related to the manipulation of Partitioned data, exposed as Rows
+package partition

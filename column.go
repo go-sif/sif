768952c884db8c8555ad40b5453998ea4dbd47ee
Sean McIntyre <s.mcintyre@xverba.ca>
2020-02-17 17:18:59 -0500

    Comment

--- column.go
+++ column.go
@@ -6,6 +6,6 @@ type Column interface {
 	Clone() Column         // Clone returns a copy of this Column
 	Index() int            // Index returns the index of this Column within a Schema
 	SetIndex(newIndex int) // Modifies the Index of this Column within a Schema
-	Start() int            // Start returns the Start position of this Column within a Row
+	Start() int            // Start returns the Start (byte) position of this Column within a Row. Not relevant for variable-length columns.
 	Type() ColumnType      // Type returns the ColumnType of this Column
 }

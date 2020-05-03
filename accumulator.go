bbb3cdd35dbd77fd26a7e838505ec0256b62073c
Sean McIntyre <s.mcintyre@xverba.ca>
2020-05-03 01:55:06 -0400

    Adding additional documentation for Accumulators

--- accumulator.go
+++ accumulator.go
@@ -2,9 +2,13 @@ package sif
 
 // An Accumulator is an alternative reduction technique, which siphons data from
 // Partitions into a custom data structure. The result is itself an Accumulator,
-// rather than a series of Partitions, thus ending the job. The advantage,
-// however, is full control over the reduction technique, which can yield
-// substantial performance benefits.
+// rather than a series of Partitions, thus ending the job (no more operations may)
+// be performed against the data. The advantage, however, is full control over the
+// reduction technique, which can yield substantial performance benefits.
+// As reduction is performed locally on all workers, then worker results are
+// all reduced on the Coordinator, Accumulators are best utilized for smaller
+// results. Distributed reductions via Reduce() are more efficient when
+// there is a large reduction result (e.g. a large number of buckets).
 type Accumulator interface {
 	Accumulate(row Row) error                  // Accumulate adds a row to this Accumulator
 	Merge(o Accumulator) error                 // Merge merges another Accumulator into this one
--- operations/util/accumulate.go
+++ operations/util/accumulate.go
@@ -22,7 +22,15 @@ func (s *accumulateTask) GetAccumulator() sif.Accumulator {
 	return s.acc
 }
 
-// Accumulate combines rows across workers, using a user-provided data structure
+// Accumulate is an alternative reduction technique, which siphons data from
+// Partitions into a custom data structure. The result is itself an Accumulator,
+// rather than a series of Partitions, thus ending the job (no more operations may)
+// be performed against the data. The advantage, however, is full control over the
+// reduction technique, which can yield substantial performance benefits.
+// As reduction is performed locally on all workers, then worker results are
+// all reduced on the Coordinator, Accumulators are best utilized for smaller
+// results. Distributed reductions via Reduce() are more efficient when
+// there is a large reduction result (e.g. a large number of buckets).
 func Accumulate(facc sif.AccumulatorFactory) *sif.DataFrameOperation {
 	return &sif.DataFrameOperation{
 		TaskType: sif.AccumulateTaskType,
